686dc6feedaae6ac7cb1013590854047c0397aea
Guilherme Santana <g.santossantana22@gmail.com>
2023-01-17 11:05:45 -0300

    finalização

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, Platform } from "react-native";
+import * as Print from "expo-print";
 
+const html = `
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+</head>
+<body style="text-align: center;">
+  <h1 style="font-size: 50px; font-family: Helvetica Neue; font-weight: normal;">
+    Hello Expo!
+  </h1>
+  <img
+    src="https://d30j33t1r58ioz.cloudfront.net/static/guides/sdk.png"
+    style="width: 90vw;" />
+</body>
+</html>
+`;
 export default function App() {
+  const [selectedPrinter, setSelectedPrinter] = useState();
+
+  const print = async () => {
+    // On iOS/android prints the given html. On web prints the HTML from the current page.
+    await Print.printAsync({
+      html,
+      printerUrl: selectedPrinter?.url, // iOS only
+    });
+  };
+
+  const printToFile = async () => {
+    // On iOS/android prints the given html. On web prints the HTML from the current page.
+    const { uri } = await Print.printToFileAsync({ html });
+    console.log("File has been saved to:", uri);
+    await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
+  };
+
+  const selectPrinter = async () => {
+    const printer = await Print.selectPrinterAsync(); // iOS only
+    setSelectedPrinter(printer);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Print" onPress={print} />
+      <View style={styles.spacer} />
+      <Button title="Print to PDF file" onPress={printToFile} />
+      {Platform.OS === "ios" && (
+        <>
+          <View style={styles.spacer} />
+          <Button title="Select printer" onPress={selectPrinter} />
+          <View style={styles.spacer} />
+          {selectedPrinter ? (
+            <Text
+              style={styles.printer}
+            >{`Selected printer: ${selectedPrinter.name}`}</Text>
+          ) : undefined}
+        </>
+      )}
     </View>
   );
 }
@@ -13,8 +65,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
